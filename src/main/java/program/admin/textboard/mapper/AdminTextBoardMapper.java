84f01298594dbb9b1589992defb37e396d87e649
YEJIN-JEONG <wls7367@naver.com>
2021-08-19 19:28:18 +0900

    AdminTextBoard 진행 중

--- src/main/java/program/admin/textboard/mapper/AdminTextBoardMapper.java
+++ src/main/java/program/admin/textboard/mapper/AdminTextBoardMapper.java
@@ -1,16 +1,47 @@
 package program.admin.textboard.mapper;
 
 import java.sql.SQLException;
-
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import program.common.CamelMap;
 import program.common.DataMap;
 
 @Repository
 public interface AdminTextBoardMapper {
 
+	/**************************************************
+	* @MethodName : insertSiteBoard
+	* @Description: 공지사항 등록
+	* @param paramMap
+	* @return
+	* @throws SQLException boolean
+	* @Author : Ye-Jin. Jeong
+	* @Version : 2021. 8. 18.
+	**************************************************/
 	boolean insertSiteBoard(DataMap paramMap) throws SQLException;
 
 	boolean updateSiteBoard(DataMap paramMap) throws SQLException;
+
+	/**************************************************
+	* @MethodName : getNoticeList
+	* @Description: 공지사항 리스트
+	* @return
+	* @throws SQLException List<CamelMap>
+	* @Author : Ye-Jin. Jeong
+	* @Version : 2021. 8. 18.
+	**************************************************/
+	List<CamelMap> getNoticeList() throws SQLException;
+
+	/**************************************************
+	* @MethodName : getNoticeInfo
+	* @Description: 공지사항 상세
+	* @param paramMap
+	* @return
+	* @throws SQLException CamelMap
+	* @Author : Ye-Jin. Jeong
+	* @Version : 2021. 8. 18.
+	**************************************************/
+	CamelMap getNoticeInfo(DataMap paramMap) throws SQLException;
 }
